e47f7ac11de6b48597de3e1a58ce55a837d007a2
igoigloo <jenigo123@gmail.com>
2023-11-27 00:35:08 -0500

    igo

--- Igo.py
+++ Igo.py
@@ -1,156 +1,70 @@
 import streamlit as st
 import pandas as pd
-import openai
-import streamlit as st
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
 
-
-# Set page config for better visuals
-st.set_page_config(page_title="Business Insight Generator", layout="wide", page_icon="📊")
-
-# Custom CSS for styling
-custom_css = """
-<style>
-    html, body, [class*="css"] {
-        font-family: 'Arial', sans-serif;
-        background-color: #F7F5F2;
-        color: #333;
+# Function to decode the country codes
+def decode_country_codes(data):
+    country_code_mapping = {
+        'NC23Q1PRCIND': 'India',
+        'NC23Q1PRCCHN': 'China',
+        'NC23Q1PRCPHL': 'Philippines',
+        'NC23Q1PRCNGA': 'Nigeria',
+        'NC23Q1PRCUSA': 'United States of America',
+        'NC23Q1PRCPAK': 'Pakistan',
+        'NC23Q1PRCFRA': 'France',
+        'NC23Q1PRCIRN': 'Iran',
+        'NC23Q1PRCBRA': 'Brazil',
+        'NC23Q1PRCKOR': 'South Korea',
+        'NC23Q1PRCGBR': 'United Kingdom',
+        'NC23Q1PRCMEX': 'Mexico',
+        'NC23Q1PRCJAM': 'Jamaica',
+        'NC23Q1PRCCOL': 'Colombia',
+        'NC23Q1PRCVIE': 'Vietnam',
+        'NC23Q1PRCBAN': 'Bangladesh',
+        'NC23Q1PRCMOR': 'Morocco',
+        'NC23Q1PRCALG': 'Algeria',
+        'NC23Q1PRCUKR': 'Ukraine',
+        'NC23Q1PRCHKG': 'Hong Kong',
+        # Add more mappings if necessary
     }
-    h1, h2, h3 {
-        color: #0D2F81;
-    }
-    .stButton > button {
-        background-color: #0D2F81;
-        color: white;
-        border-radius: 5px;
-        border: 1px solid #0D2F81;
-    }
-    .stButton > button:hover {
-        background-color: #0D2F81;
-        opacity: 0.8;
-    }
-    .st-bb {
-        border-bottom: 2px solid #0D2F81;
-    }
-    .stTextInput > div > div > input, .stSelectbox > div > div {
-        border-radius: 5px;
-    }
-</style>
-"""
-st.markdown(custom_css, unsafe_allow_html=True)
+    data['Country'] = data['Country'].map(country_code_mapping)
+    return data.dropna()
 
-# Initialize session states
-if 'continue_clicked' not in st.session_state:
-    st.session_state.continue_clicked = False
-if 'user_type' not in st.session_state:
-    st.session_state.user_type = None
+# Function to generate choropleth map
+def generate_choropleth(data):
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    merged = world.merge(data, left_on='name', right_on='Country', how='left')
+    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+    merged.plot(column='Count', ax=ax, legend=True, cmap='OrRd', missing_kwds={
+        "color": "lightgrey",
+        "edgecolor": "black",
+        "hatch": "///",
+        "label": "Missing values"
+    })
+    ax.set_title('Permanent Resident Count by Country in Trade Area', fontsize=15)
+    img_buf = BytesIO()
+    fig.savefig(img_buf, format='png')
+    img_buf.seek(0)
+    return Image.open(img_buf)
 
-def welcome_page():
-    st.title("Welcome to the Business Insight Generator App!")
-    st.markdown("""
-    This application allows you to upload Excel files and generates business insights based on the data.
-    The insights are generated using OpenAI's GPT-3.
-    """, unsafe_allow_html=True)
+# Streamlit UI
+st.title("Choropleth Map Generator for Permanent Residents")
 
-    user_type = st.selectbox("Select your workplace type:", ('Big Retail Firm', 'Grocery Store', 'Vendor'))
-    
-    if st.button("Continue"):
-        st.session_state.continue_clicked = True
-        st.session_state.user_type = user_type
+# File uploader for Excel files
+uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
+if uploaded_file is not None:
+    # Process the Excel file
+    data = pd.read_excel(uploaded_file, skiprows=4)  # Skip header rows
 
-def read_excel(uploaded_file):
-    if uploaded_file.name.endswith('.xlsx') or uploaded_file.name.endswith('.xls'):
-        return pd.read_excel(uploaded_file)
-    else:
-        st.error("Uploaded file is not an Excel file.")
-        return pd.DataFrame()
+    # Updating column names based on the actual structure of your Excel file
+    data.columns = ['Country', 'Description', 'Count', 'Percent', 'Base Count', 'Base Percent', 'Percent Penetration', 'Index']
+    data = data[['Country', 'Count']]
+    data = decode_country_codes(data)
 
-def process_data_for_openai(dataframes):
-    combined_data = pd.concat(dataframes, ignore_index=True)
-    return combined_data.to_string()
-
-def get_openai_insight(prompt, data_context, user_type):
-    openai.api_key = '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'
-    
-    tailored_context = f"As a {user_type}, " + data_context
-    full_prompt = f"{tailored_context}\n\n{prompt}\n"
-    
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=full_prompt,
-        max_tokens=150
-    )
-    return response.choices[0].text.strip()
-
-def main():
-    if not st.session_state.continue_clicked:
-        welcome_page()
-    else:
-        st.title(f"Business Insight Generator for {st.session_state.user_type}")
-
-        uploaded_files = st.file_uploader("Upload Excel files", type="xlsx", accept_multiple_files=True)
-
-        data_context = ""
-        if uploaded_files:
-            all_dfs = [read_excel(uploaded_file) for uploaded_file in uploaded_files]
-            data_context = process_data_for_openai(all_dfs)
-
-        # Generate insights based on provided categories
-        if data_context:
-            if st.button("Generate Business Idea"):
-                with st.spinner('Generating business idea...'):
-                    prompt_part1 = get_openai_insight(
-                        "As a chief business strategist, analyze the provided data to identify a business idea "
-                        "that caters specifically to the unique needs and preferences of the selected demographic group. "
-                        "Focus on how the metrics in the dataset reveal specific opportunities or needs within the group. "
-                        "Your initial output should include: "
-                        "1. The name of the business idea. "
-                        "2. A description of the business idea. "
-                        , data_context, st.session_state.user_type
-                    )
-
-                    prompt_part2 = get_openai_insight(
-                        "Continuing with the business idea identified, detail the specific metrics from the dataset that support this idea. "
-                        "Explain how these metrics demonstrate the viability and potential success of the business idea in addressing the "
-                        "needs and preferences of the target demographic group. Include: "
-                        "1. The metrics used from the dataset, specifying with numbers."
-                        , data_context, st.session_state.user_type
-                    )
-
-                    prompt_part3a = get_openai_insight(
-                        "Based on the previously identified business idea, outline the practical steps for implementing this idea "
-                        "within the trade region. Focus on the initial stages of implementation, considering the specific requirements "
-                        "and challenges of the selected trade area. This part should include: "
-                        "1. Practical steps for implementing the business idea in the selected trade area."
-                        , data_context, st.session_state.user_type
-                    )
-
-                    prompt_part3b = get_openai_insight(
-                        "Continuing with the development of the business implementation plan, consider external market factors, trends, "
-                        "and demographic insights that support the viability and success of the business idea. Develop specific strategies "
-                        "to engage with the target demographic group, based on the identified needs and preferences. This part should cover: "
-                        "1. How external market factors, trends, and demographic insights support the viability and success of the business idea. "
-                        "2. Specific strategies to engage with the target demographic group based on the identified needs and preferences."
-                        , data_context, st.session_state.user_type
-                    )
-
-
-                    idea = prompt_part1 + prompt_part2 + prompt_part3a + prompt_part3b
-                    st.write("Business Idea:", idea)
-
-            if st.button("Generate Marketing Strategy"):
-                with st.spinner('Generating marketing strategy...'):
-                    strategy = get_openai_insight("Craft a marketing strategy that targets the diverse demographic composition detailed in the data, with a special focus on the significant permanent resident communities. Include culturally tailored messaging, appropriate media channels for these segments, and marketing tactics that resonate with their cultural values and consumption patterns. Propose ways to measure the impact and effectiveness of these culturally nuanced marketing efforts.", data_context, st.session_state.user_type)
-                    st.write("Marketing Strategy:", strategy)
-
-            if st.button("Identify Best Products/Brands to Launch"):
-                with st.spinner('Identifying best products/brands...'):
-                    products = get_openai_insight("Analyze the demographic data to recommend products or brands that would appeal to the diverse community composition, especially focusing on the larger groups of permanent residents. Highlight potential products or services that align with the cultural preferences, lifestyle, and consumption habits of these groups. Also, consider any gaps in the current market offerings that these products or brands could fill. Please list off real world products and brands", data_context, st.session_state.user_type)
-                    st.write("Best Products/Brands:", products)
-
-            if st.button("Suggest CSR Initiatives"):
-                with st.spinner('Suggesting CSR initiatives...'):
-                    csr = get_openai_insight("Analyze the demographic data to recommend products or brands that would appeal to the diverse community composition, especially focusing on the larger groups of permanent residents from India and China. Highlight potential products or services that align with the cultural preferences, lifestyle, and consumption habits of these groups. Also, consider any gaps in the current market offerings that these products or brands could fill.", data_context, st.session_state.user_type)
-                    st.write("CSR Initiatives:", csr)
-
-if __name__ == "__main__":
-    main()
+    # Generate and display the choropleth map
+    st.write("Choropleth Map:")
+    map_image = generate_choropleth(data)
+    st.image(map_image)
